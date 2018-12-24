184e603e4a5a2daca2e6c0c0250e388260d798af
TianTian-DouBa <fanxc1@hotmail.com>
2018-12-24 11:14:00 +0800

    initial

---
+++ XF_NueBee.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+	"net"
+)
+
+func GetMac() (mac_addresses []string) {
+	net_interfaces, err := net.Interfaces()
+	if err != nil {
+		fmt.Printf("fail to get network interfaces: %v", err)
+		return nil
+	}
+	for _, net_interface := range net_interfaces {
+		mac_address := net_interface.HardwareAddr.String()
+		if len(mac_address) == 0 {
+			continue
+		}
+		mac_addresses = append(mac_addresses, mac_address)
+	}
+	return mac_addresses
+}
+
+func main() {
+	fmt.Printf("mac addresses: %v\n", GetMac())
+	fmt.Printf("mac addresses: %q\n", GetMac())
+}
